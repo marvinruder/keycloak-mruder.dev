006d972b53f7e3bcda284fcac92cc0efc3663b5e
Marvin A. Ruder <signed@mruder.dev>
2024-07-26 18:47:02 +0000

    Set `accountThemeImplementation` to `none`
    Signed-off-by: Marvin A. Ruder <signed@mruder.dev>

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { keycloakify } from "keycloakify/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), keycloakify({})]
+    plugins: [react(), keycloakify({ accountThemeImplementation: "none" })]
 });
